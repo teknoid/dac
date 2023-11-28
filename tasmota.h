bcc5e69232e019aa5bad7d2af97176fe8dcbc8fb
hje <hje@prime.jecons.net>
2023-11-28 12:16:41 +0100

    plug 2+3

--- tasmota.h
+++ tasmota.h
@@ -3,8 +3,8 @@
 
 // Relays
 #define PLUG1			0x810F43
-#define PLUG2			0x123456
-#define PLUG3			0x123456
+#define PLUG2			0x81420A
+#define PLUG3			0x814D47
 #define PLUG4			0x83185A
 #define HOFLICHT		0x2FEFEE
 
