69acbe339fa8946225a619972c55048bae950295
hje <hje@prime.jecons.net>
2024-04-23 21:41:01 +0200

    function order to detect auto power release

--- fronius.c
+++ fronius.c
@@ -541,6 +541,50 @@ static void steal_power() {
 	state->greedy += state->steal;
 }
 
+static int check_response(device_t *d) {
+	int delta = state->load - get_history(-1)->load;
+
+	// response OK -> continue
+	if (d->standby != -1 && delta) {
+		xdebug("FRONIUS response OK for %s, delta load is %d", d->name, delta);
+		return 0;
+	}
+
+	// standby check was negative - we got a response -> continue
+	if (d->standby == -1 && delta) {
+		xdebug("FRONIUS standby check negative for %s, delta load is %d", d->name, delta);
+		d->standby = 0;
+		return 0;
+	}
+
+	// standby check was positive set device into standby and continue
+	if (d->standby == -1 && !delta) {
+		xdebug("FRONIUS standby check positive for %s, no delta load --> entering standby", d->name);
+		(d->set_function)(d, 0);
+		d->standby = 1;
+		return 0;
+	}
+
+	// do the standby check in next round
+	xdebug("FRONIUS do standby check for %s", d->name);
+	d->standby = -1;
+	if (d->adjustable) {
+		// adjustable device - do a big ramp
+		if (d->power > 50)
+			return (d->set_function)(d, 25);
+		else
+			return (d->set_function)(d, 75);
+	} else {
+		// dumb device - toggle
+		if (d->power)
+			return (d->set_function)(d, 0);
+		else
+			return (d->set_function)(d, 1);
+	}
+
+	return 0;
+}
+
 static void calculate_state() {
 	// get 3x history back
 	state_t *h1 = get_history(-1);
@@ -626,48 +670,6 @@ static void calculate_state() {
 	char message[128];
 	snprintf(message, 128, "avg:%d var:%lu kf:%d kt:%d", avg, var, kf, kt);
 	print_power_status(message);
-}
-
-static int check_device_response(device_t *d) {
-	// response OK -> continue
-	if (d->standby != -1 && state->dload) {
-		xdebug("FRONIUS response OK for %s, delta load is %d", d->name, state->dload);
-		return 0;
-	}
-
-	// standby check was negative - we got a response -> continue
-	if (d->standby == -1 && state->dload) {
-		xdebug("FRONIUS standby check negative for %s, delta load is %d", d->name, state->dload);
-		d->standby = 0;
-		return 0;
-	}
-
-	// standby check was positive set device into standby and continue
-	if (d->standby == -1 && !state->dload) {
-		xdebug("FRONIUS standby check positive for %s, no delta load --> entering standby", d->name);
-		(d->set_function)(d, 0);
-		d->standby = 1;
-		return 0;
-	}
-
-	// do the standby check in next round
-	xdebug("FRONIUS do standby check for %s", d->name);
-	d->standby = -1;
-	if (d->adjustable) {
-		// adjustable device - do a big ramp
-		if (d->power > 50)
-			return (d->set_function)(d, 25);
-		else
-			return (d->set_function)(d, 75);
-	} else {
-		// dumb device - toggle
-		if (d->power)
-			return (d->set_function)(d, 0);
-		else
-			return (d->set_function)(d, 1);
-	}
-
-	return 0;
 }
 
 static void calculate_next_round() {
@@ -773,19 +775,22 @@ static void* fronius(void *arg) {
 		if (ret != 0)
 			xlog("FRONIUS Error calling Fronius7 API: %d", ret);
 
+		// check response from previous ramp
+		if (device && check_response(device)) {
+			wait = WAIT_NEXT;
+			continue;
+		}
+
 		// calculate actual state
 		calculate_state();
 
-		// check response from last device adjustment
-		if (device) {
-			if (check_device_response(device)) {
-				wait = WAIT_NEXT;
-				continue;
-			} else
-				device = 0;
-		}
+		// something switched off which was not initiated by us
+		// TODO do something with that information, e.g. initiate a standby check on all ramped devices
+		if (!device && state->dload > BASELOAD)
+			xdebug("FRONIUS power released by someone: %d", state->dload);
 
 		// ramp up/down devices depending on if we have surplus or not
+		device = 0;
 		if (state->greedy)
 			device = ramp();
 
