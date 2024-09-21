8b5f291f94f5943129383006e4477ebf3a3f1c50
hje <hje@q.jecons.net>
2024-09-21 18:07:23 +0200

    fix ramp dumb 50% more when distortion

--- fronius-api.c
+++ fronius-api.c
@@ -470,7 +470,7 @@ static int ramp_adjustable(device_t *d, int power) {
 }
 
 static int ramp_dumb(device_t *d, int power) {
-	int min = d->load + state->distortion ? d->load / 2 : 0;
+	int min = d->load + (state->distortion ? d->load / 2 : 0); // 50% more when distortion
 	xdebug("FRONIUS ramp_dumb() %s %d (min %d)", d->name, power, min);
 
 	// keep on as long as we have enough power and device is already on
