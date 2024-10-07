4b67a4ae91681f0a8f6fe1aec850f898c508b19c
hje <hje@q.jecons.net>
2024-10-07 20:18:17 +0200

    fix discharge calc

--- fronius-api.c
+++ fronius-api.c
@@ -977,14 +977,13 @@ static void hourly(time_t now_ts) {
 	// calculate akku discharge rate for last hour
 	if (pstate->pv == 0 && pstate->soc < 900 && pstate->soc > 100) {
 		if (discharge_soc && discharge_ts) {
+			// calculate
 			int seconds = now_ts - discharge_ts;
 			int start = AKKU_CAPACITY * discharge_soc / 1000;
 			int end = AKKU_CAPACITY * pstate->soc / 1000;
 			int lost = start - end;
 			gstate->discharge = discharge[now->tm_hour] = lost;
 			xlog("FRONIUS calculated akku discharge rate for last hour: %d (seconds=%d start=%d end=%d lost=%d", gstate->discharge, seconds, start, end, lost);
-			discharge_soc = pstate->soc;
-			discharge_ts = now_ts;
 
 			// dump hourly collected discharge rates
 			char message[LINEBUF], value[6];
@@ -994,6 +993,9 @@ static void hourly(time_t now_ts) {
 			strcat(message, value);
 			xdebug(message);
 		}
+		// update for next calculation
+		discharge_soc = pstate->soc;
+		discharge_ts = now_ts;
 	} else
 		discharge_soc = discharge_ts = 0;
 
