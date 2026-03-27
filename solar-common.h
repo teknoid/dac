e3ee336f7ea0a793d75159884696c754fc8aefe8
Heiko Jehmlich <hje@jecons.de>
2026-03-27 22:16:37 +0100

    fix json labels

--- solar-common.h
+++ solar-common.h
@@ -184,7 +184,7 @@ struct _counter {
 // 24/7 gstate history slots
 typedef struct _gstate gstate_t;
 #define GSTATE_SIZE		(sizeof(gstate_t) / sizeof(int))
-#define GSTATE_HEADER	"    pv ↑grid ↓grid today  tomo   sod   eod   soc   ttl  succ  foca avail  need  minu  surv flags"
+#define GSTATE_HEADER	"    pv ugrid dgrid today  tomo   sod   eod   soc   ttl  succ  foca avail  need  minu  surv flags"
 struct _gstate {
 	int pv;
 	int produced;
